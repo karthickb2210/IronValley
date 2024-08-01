7569b0ed7f7e16a879be4cd37ae1b80d6d80f545
karthickb2210 <karthick.b@tmachinesoftware.com>
2024-08-01 17:12:24 +0530

    removed dark theme from testimonials

--- src/Components/EnVita/Testienvita.jsx
+++ src/Components/EnVita/Testienvita.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 function Testienvita() {
   return (
     <div>
-      <section className=" relative dark:text-gray-800 h-[100vh] mb-44">
+      <section className=" relative text-gray-800 h-[100vh] mb-44">
         <div className="container px-6 py-12 mx-auto">
           <div className="grid items-center gap-4 xl:grid-cols-5">
             <div className="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 md:text-left">
@@ -30,7 +30,7 @@ function Testienvita() {
             <div className="p-6 text-[15px] xl:col-span-3">
               <div className="grid gap-4 md:grid-cols-2">
                 <div className="grid content-center gap-4">
-                  <div className="p-6 rounded shadow-md dark:bg-gray-50">
+                  <div className="p-6 rounded shadow-md bg-gray-50">
                     <p>
                       ...I tried this on my old honda activa and was suprised to
                       see the immediate difference in the noise. Now I am loving
@@ -42,16 +42,16 @@ function Testienvita() {
                       <img
                         src="https://t3.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
                         alt=""
-                        className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500"
+                        className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500"
                       />
 
                       <div>
                         <p className="text-lg font-semibold">Mannar</p>
-                        <p className="text-sm dark:text-gray-600">Villupuram</p>
+                        <p className="text-sm text-gray-600">Villupuram</p>
                       </div>
                     </div>
                   </div>
-                  <div className="p-6 rounded shadow-md dark:bg-gray-50 mb-10">
+                  <div className="p-6 rounded shadow-md bg-gray-50 mb-10">
                     <p>
                       …I am the owner and driver of Leyland Taurus 10 wheeler,
                       my friend reffered this product and I added them between
@@ -69,7 +69,7 @@ function Testienvita() {
                       />
                       <div>
                         <p className="text-lg font-semibold">Sivaramkrishnan</p>
-                        <p className="text-sm dark:text-gray-600">
+                        <p className="text-sm text-gray-600">
                           Parangipettai
                         </p>
                       </div>
@@ -77,7 +77,7 @@ function Testienvita() {
                   </div>
                 </div>
                 <div className="grid content-center gap-4">
-                  <div className="p-6 rounded shadow-md dark:bg-gray-50 ">
+                  <div className="p-6 rounded shadow-md bg-gray-50 ">
                     <p>
                       …Recently purchased this item after a friend of mine told
                       that it's giving a good result. So, just tried as soon I
@@ -93,15 +93,15 @@ function Testienvita() {
                       <img
                         src="https://t3.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
                         alt=""
-                        className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500"
+                        className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500"
                       />
                       <div>
                         <p className="text-lg font-semibold">Rajiv Raj</p>
-                        <p className="text-sm dark:text-gray-600">Nolambur</p>
+                        <p className="text-sm text-gray-600">Nolambur</p>
                       </div>
                     </div>
                   </div>
-                  <div className="p-6 rounded shadow-md dark:bg-gray-50 ">
+                  <div className="p-6 rounded shadow-md bg-gray-50 ">
                     <p>
                       …I run a car mechanic workshop and I use fully synthetic
                       engine oils for all the customers vehicles. I know the
@@ -114,12 +114,12 @@ function Testienvita() {
                       <img
                         src="https://t3.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
                         alt=""
-                        className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500"
+                        className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500"
                       />
 
                       <div>
                         <p className="text-lg font-semibold">Shiva</p>
-                        <p className="text-sm dark:text-gray-600">
+                        <p className="text-sm text-gray-600">
                           Kolathur, Chennai{" "}
                         </p>
                       </div>
