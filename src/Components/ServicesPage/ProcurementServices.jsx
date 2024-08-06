06dd01aabc0bfd3a037ab23c27057a6c9182904d
karthickb2210 <karthick.b@tmachinesoftware.com>
2024-08-06 20:44:54 +0530

    more companies added

--- src/Components/ServicesPage/ProcurementServices.jsx
+++ src/Components/ServicesPage/ProcurementServices.jsx
@@ -20,6 +20,31 @@ import kendeil from "./Companylogo/kendeil.png"
 import seimens from "./Companylogo/seimens.png"
 import smc from "./Companylogo/smc.png"
 
+import m3 from "./Companylogo/26/3m.png"
+import alvarado from "./Companylogo/26/alvarado.png"
+import amphenol from "./Companylogo/26/amphenol.png"
+import ashcroft from "./Companylogo/26/ashcroft.png"
+import carraro from "./Companylogo/26/carraro.png"
+import cooper from "./Companylogo/26/cooper.jpg"
+import crouzet from "./Companylogo/26/crouzet.png"
+import dpv from "./Companylogo/26/dpv.png"
+import euromotori from "./Companylogo/26/euromotori.png"
+import fender from "./Companylogo/26/fender.png"
+import flender from "./Companylogo/26/flender.png"
+import hadef from "./Companylogo/26/hadef.png"
+import imap3 from "./Companylogo/26/imap3.png"
+import jeteco from "./Companylogo/26/jeteco.png"
+import mercer from "./Companylogo/26/mercer.jpg"
+import moxa from "./Companylogo/26/moxa.png"
+import neway from "./Companylogo/26/neway.png"
+import nordlock from "./Companylogo/26/nordlock.png"
+import phoenix from "./Companylogo/26/phoeniccontact.png"
+import pkvalve from "./Companylogo/26/pkvalve.png"
+import storz from "./Companylogo/26/storz.png"
+import trelleborg from "./Companylogo/26/trelleborg.png"
+import velan from  "./Companylogo/26/velan.png"
+import xylem from "./Companylogo/26/xylem.png"
+
 
 
 export default function ProcurementServices() {
@@ -37,34 +62,56 @@ export default function ProcurementServices() {
         </p>
       </div>
       <div className="text-center">
-        <h2 className="text-4xl font-bold mb-12">Our Clients</h2>
-        <div className=" grid lg:grid-cols-4  xl:grid-cols-5 md:grid-cols-3   sm:grid-cols-2 items-center gap-16  ">
+        <h2 className="text-4xl font-bold mb-12">Brands We Have Served</h2>
+        <div className=" grid lg:grid-cols-8 xl:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 items-center gap-10">
         
-          <img  src={baumer} alt="1" className="rounded-xl max-w-40 object-cover  transition-transform duration-400 hover:scale-110 bg-white "/>
-          
-          <img  src={emerson}  alt="1" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img  src={honeywell} alt="1" className="rounded-xl max-w-40 object-cover  transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img  src={ferris} alt="1" className="rounded-xl max-w-40 max-h-40 object-scale-down  transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={kei} alt="Client 2" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={peperrl} alt="Client 2" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={robertshaw} alt="Client 2" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={samson} alt="Client 1" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={wikai} alt="Client 2" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={baker} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
+          <img  src={baumer} alt="1" className="rounded-xl max-w-30 max-h-30 object-cover  transition-transform duration-300 hover:scale-110 bg-white "/>
+          <img  src={emerson}  alt="1" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img  src={honeywell} alt="1" className="rounded-xl max-w-30 object-cover  transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img  src={ferris} alt="1" className="rounded-xl max-w-30 max-h-30 object-scale-down  transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={kei} alt="Client 2" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={peperrl} alt="Client 2" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={robertshaw} alt="Client 2" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={samson} alt="Client 1" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={wikai} alt="Client 2" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={baker} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
         
-          <img src={abb} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={allenbradley} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={conductix} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={illinois} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={interapp} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={rittal} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={seimens} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={kendeil} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
-          <img src={smc} alt="Client 3" className="max-w-40 max-h-40 rounded-xl object-cover transition-transform duration-400 hover:scale-110 bg-white"/>
+          <img src={abb} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={allenbradley} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={conductix} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={illinois} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={interapp} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={rittal} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={seimens} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={kendeil} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={smc} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+
+          <img src={flender} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={alvarado} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={amphenol} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={ashcroft} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={carraro} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={cooper} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={crouzet} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={dpv} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={euromotori} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={fender} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={m3} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={hadef} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={imap3} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={jeteco} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={mercer} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={moxa} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={neway} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={nordlock} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={phoenix} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={pkvalve} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={storz} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={trelleborg} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={velan} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
+          <img src={xylem} alt="Client 3" className="max-w-30 max-h-30 rounded-xl object-cover transition-transform duration-300 hover:scale-110 bg-white"/>
 
 
-        
-          {/* Add more images as needed */}
         </div>
       </div>
     </div>
